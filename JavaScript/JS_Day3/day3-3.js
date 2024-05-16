c9c523f819ef593bd387548c3a528c2ba926d58c
Leoryu90 <thetein@gmail.com>
2024-05-16 17:20:23 +0900

    :memo: 20240516 JS day3 more

---
+++ JavaScript/JS_Day3/day3-3.js
@@ -0,0 +1,11 @@
+const arr = [1, 11, 3, 22, 2, 33];
+console.log(arr.sort());
+
+const arr = [1, 11, 3, 22, 2, 33];
+console.log(arr.sort((a, b) => a - b));
+
+const arr = ['참외', '키위', '감귤'];
+undefined
+arr.forEach((item) => {
+    console.log(item)
+})
