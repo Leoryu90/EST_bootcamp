9be91e45075588247c4cfd7e830213c45dfa8d6b
Leoryu90 <thetein@gmail.com>
2024-05-18 14:20:39 +0900

    :memo: 20240517 JS day4 ru

--- JavaScript/JS_Day4/Day4_constructor.js
+++ JavaScript/JS_Day4/Day4_constructor.js
@@ -1,9 +1,11 @@
-const person = {
-    name: '재현',
-    age: 20,
-    gender: 'male'
-};
-
-for (let key in person) {
-    console.log(`${key}: ${person[key]}`);
+function NewFactory(name, age){
+    this.name = name;
+    this.age = age;
+    this.sayYourName = function(){
+        console.log(`제 이름은 ${this.name}입니다. 반갑습니다..`);
+    }
 }
+
+let robot1 = new NewFactory('우루루까꿍', 13);
+
+console.log(robot1);
